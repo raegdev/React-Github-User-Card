97e1da7d4643ef912104f816e47af8724552bdbc
rae-glazier <glazier.home.life@gmail.com>
2019-12-03 17:56:50 -0600

    user card completed as it follower card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,14 +6,23 @@ import axios from 'axios';
 class App extends React.Component {
 
   state = {
-    cards: []
+    userCard: [],
+    followerCard: []
   }
 
   componentDidMount(){
+    console.log('cDM is running')
     //axios to pull data from github
     axios.get('https://api.github.com/users/Rae-Glazier')
       .then(res => {
         console.log(res);
+
+        this.setState({ userCard: res.data });
+      });
+
+    axios.get('https://api.github.com/users/Rae-Glazier/followers')  
+      .then(res => {
+        this.setState({ followerCard: res.data })
       })
   }
 
@@ -22,18 +31,31 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+         <h1> Github User Card</h1>
         </header>
+        
+        <div className='user-card'>
+          <h2>Name: {this.state.userCard.name}</h2>
+          <h3>Username: {this.state.userCard.login} </h3>
+          <h3>Location: {this.state.userCard.location}</h3>
+          <h3>Bio: {this.state.userCard.bio}</h3>
+        </div>
+        
+        <div className='follower-card'>
+          {this.state.followerCard.map ( (follower) => {
+            return(
+              <>
+                <h2>Name: {follower.name}</h2>
+                <h3>Username: {follower.login} </h3>
+                <h3>Location: {follower.location}</h3>
+                <h3>Bio: {follower.bio}</h3>
+              </>
+              )
+            
+          })}
+        </div>
+       
+
       </div>
     );
 
